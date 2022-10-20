b9c26f35013476036eb0d5ac32f464fe5e73aee6
diegocoelgolins <diego.dev.design@gmail.com>
2022-10-20 09:54:42 -0300

    project finished

---
+++ src/main.js
@@ -0,0 +1,132 @@
+import "./css/index.css"
+import IMask from "imask"
+
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
+const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
+const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img ')
+
+function setCardType(type) {
+  const colors = {
+    visa: ['#436099', '#2D57F2'],
+    mastercard: ['#DF6F29', '#C69347'],
+    default: ['black', 'gray']
+  }
+  // pegando as cores e alterando para cada tipo de bandeira
+  ccBgColor01.setAttribute('fill', colors[type][0])
+  ccBgColor02.setAttribute('fill', colors[type][1])
+  ccLogo.setAttribute('src', `cc-${type}.svg`)
+}
+// função passada para global
+globalThis.setCardType = setCardType
+
+// security code
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+  mask: "0000"
+}
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      // pega o ano atual, transforma em string e coloca os dois ultimos digitos
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    },
+
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+  }
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector('#card-number')
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardType: "visa"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      // expressão regular
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardType: "mastercard"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardType: "default"
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+    // console.log(foundMask)
+    return foundMask
+  },
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+// evento do botão - adicionar
+const addButton = document.querySelector('#ad-card')
+addButton.addEventListener('click', () => {
+  alert('cartão adicionado')
+})
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault()
+})
+
+// função para mudar o nome no cartão
+const cardHolder = document.querySelector('#card-holder')
+cardHolder.addEventListener('input', () => {
+  const ccHolder = document.querySelector('.cc-holder .value')
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? 'FULANO DA SILVA' : cardHolder.value
+})
+
+// função para mudar o número no cvc
+securityCodeMasked.on('accept', () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector('.cc-security .value')
+
+  ccSecurity.innerText = code.length === 0 ? '***' : code
+}
+
+// função para mudar o número no cartão
+cardNumberMasked.on('accept', () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardType
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector('.cc-number')
+
+  ccNumber.innerText = number.length === 0 ? 'XXXX XXXX XXXX XXXX' : number
+}
+
+// função para mudar data no cartão
+expirationDateMasked.on('accept', () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector('.cc-expiration .value')
+
+  ccExpiration.innerText = date.length === 0 ? '00/00' : date
+}
